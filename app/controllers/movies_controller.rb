a3ef0d18cf5e22aa0609a641c08ebb431171d1c8
wcfulmer <wfulmer1@uncc.edu>
2017-03-02 21:59:01 +0000

    done with section 4.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,24 @@ class MoviesController < ApplicationController
     def new
        @movie = Movie.new
     end
+    
+    def create
+        #@movie = Movie.create!(params[:movie]) #old way
+        @movie = Movie.create!(movie_params)  # new way
+        flash[:notice] = "#{@movie.title} was successfully created."
+        redirect_to movies_path
+    end
+    
+    def update
+        @movie = Movie.find params[:id]
+        #@movie.update_attributes!(params[:movie])  # old way
+        @movie.update_attributes!(movie_params)  # new way  
+        flash[:notice] = "#{@movie.title} was successfully updated."
+        redirect_to movie_path(@movie)
+    end
+
+
+    
     # add below all other methods
 private
 
